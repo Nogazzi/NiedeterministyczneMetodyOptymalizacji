0c5b3f6a86c7625d7ab6d30cb34477ade4ed8242
Nogazzi <p.noganski@gmail.com>
2017-03-12 18:34:22 +0100

    fixed Cauchy generator

--- src/generatory/RozkladCauchyego.java
+++ src/generatory/RozkladCauchyego.java
@@ -6,18 +6,18 @@ import java.util.Random;
  * Created by Nogaz on 27.02.2017.
  */
 public class RozkladCauchyego implements RandomGenerator {
-    double sigma = 2.0;
+    double sigma = 1.0;
     @Override
     public double getRandomDouble() {
         Random random = new Random();
         double d = random.nextDouble();
-        double result = this.sigma*Math.tan(Math.PI * (d-0.5));
+        double result = Math.tan(Math.PI * (d-0.5));
         return result;
     }
-    public double getRandomDouble(double sigma) {
+    public double getRandomDouble(double ceil) {
         Random random = new Random();
         double d = random.nextDouble();
-        double result = this.sigma*Math.tan(Math.PI * (d-0.5));
+        double result = ceil*Math.tan(Math.PI * (d-0.5));
         return result;
     }
 
