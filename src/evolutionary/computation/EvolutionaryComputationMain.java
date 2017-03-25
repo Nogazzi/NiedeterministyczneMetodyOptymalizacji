eb30d6741b7cbd1bc0a6a08471ae38f200d19b3d
Nogazzi <p.noganski@gmail.com>
2017-03-25 17:29:02 +0100

    [Evolutionary Computation] mutate population

--- src/evolutionary/computation/EvolutionaryComputationMain.java
+++ src/evolutionary/computation/EvolutionaryComputationMain.java
@@ -4,6 +4,7 @@ import generatory.RandomGenerator;
 import generatory.RozkladJednostajny;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by Nogaz on 25.03.2017.
@@ -11,15 +12,23 @@ import java.util.ArrayList;
 public class EvolutionaryComputationMain {
 
     
-    
+    private static double sigma = 1.0d;
+    private static double tau = 1.0d;
+    private static double tauPrin = 1.0d;
 
     public static void main(String[] args){
         final int firstPopulationSize = 40;
+        //generate population
         ArrayList<Individual> population = generatePopulation(firstPopulationSize);
+        //recombinate population - weź tych lepszych
+        ArrayList<Individual> selectedPopulation = selectPopulation(population);
+        //mutate selected - stworz potomkow wybrancow
+        ArrayList<Individual> mutatedPopulation = mutatePopulation(selectedPopulation);
+        //populacja wybranych i ich potomkow
+        ArrayList<Individual> newPopulation = new ArrayList<Individual>();
+        newPopulation.addAll(selectedPopulation);
+        newPopulation.addAll(mutatedPopulation);
 
-        for (Individual unit: population) {
-            System.out.println(unit);
-        }
 
     }
 
@@ -33,10 +42,40 @@ public class EvolutionaryComputationMain {
     }
 
     private static ArrayList<Individual> recombinatePopulation(final ArrayList<Individual> population){
-        for( int i = 0 ; i < population.size() ; ++i ){
+        for( int i = 0 ; i < population.size() ; i++ ){
             population.get(i);
         }
         return null;
+    }
+
+    private static ArrayList<Individual> selectPopulation(final ArrayList<Individual> population){
+        final ArrayList<Individual> newPopulation = new ArrayList<Individual>();
+        for( int i = 0 ; i < population.size() ; i+=2 ){
+            if( population.get(i).getResult() < population.get(i+1).getResult() ){
+                newPopulation.add(population.get(i));
+            }else{
+                newPopulation.add(population.get(i+1));
+            }
+        }
+        return newPopulation;
+    }
+
+    private static ArrayList<Individual> mutatePopulation(final ArrayList<Individual> population){
+        final ArrayList<Individual> mutatedPopulation = new ArrayList<Individual>();
+        for ( Individual individual: population) {
+            mutatedPopulation.add(mutateIndividual(individual));
+        }
+        return mutatedPopulation;
+    }
+
+    private static Individual mutateIndividual(final Individual individual/*, final double sigma*/){
+        Random generator = new Random();
+        final Individual newIndividual;
+        sigma = sigma * Math.exp(tau*generator.nextDouble() + tauPrin*generator.nextDouble());
+        double newX1 = individual.getX1() + generator.nextDouble()*sigma;
+        double newX2 = individual.getX2() + generator.nextDouble()*sigma;
+        newIndividual = new Individual(newX1, newX2);
+        return newIndividual;
     }
 
 }
--- src/evolutionary/computation/Individual.java
+++ src/evolutionary/computation/Individual.java
@@ -11,17 +11,19 @@ public class Individual {
     public Individual(double x1, double x2){
         this.x1 = x1;
         this.x2 = x2;
-        computeResult();
+        evaluate();
     }
 
-    private void computeResult(){
+    private void evaluate(){
         result = 2.0d*x1*x1 - 1.05d*x1*x1*x1*x1 + (x1*x1*x1*x1*x1*x1)/6.0d + x1*x2 + x2*x2;
     }
 
     public double getX1(){ return x1; }
     public double getX2(){ return x2; }
     public double getResult(){ return result; }
+    public void mutate(){
 
+    }
     @Override
     public String toString() {
         return "X1: " + x1 + ", X2: " + x2 + ", result: " + result;
